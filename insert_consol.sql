493d5d5fc6d6dae793b9456ebae5be9e26296789
я <unanswered2012@gmail.com>
2022-12-31 05:24:41 +0300

    adress commits

--- insert_consol.sql
+++ insert_consol.sql
@@ -25,4 +25,30 @@ INSERT INTO adress (adress_id, country, city, street, house) VALUES (22, 'Finlan
 INSERT INTO adress (adress_id, country, city, street, house) VALUES (23, 'Finland', 'Uus-Atte', 'Isomäkikierros', 596);
 INSERT INTO adress (adress_id, country, city, street, house) VALUES (24, 'Finland', 'Pohjois-Christalampi', 'Alajärvikierros', 338);
 INSERT INTO adress (adress_id, country, city, street, house) VALUES (25, 'Finland', 'Uus-Anissa', 'Emmaliinakulma', 883);
+---PEROSONS COMMIT--
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1337, 'Valentina', 'Golubkina', 'student', 1, 'female');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1338, 'Karina', 'Lapuhina', 'student', 2, 'female');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1339, 'Inna', 'Hohlova', 'student', 3, 'female');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1340, 'Alisa', 'Grabar', 'student', 4, 'female');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1341, 'Zinaida', 'Fedoseeva', 'student', 5, 'female');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1342, 'Gerasim', 'Zutskov', 'student', 6, 'male');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1343, 'Kseniya', 'Assorova', 'student', 7, 'female');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1344, 'Stepan', 'Vyazmitin', 'student', 8, 'male');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1345, 'Ignat', 'Korshikov', 'student', 9, 'male');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1346, 'Serafima', 'Kapilyshnaya', 'student', 10, 'female');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1347, 'Candace', 'Witting', 'teacher', 11, 'female');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1348, 'Dawson', 'Purdy', 'student', 12, 'male');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1349, 'Annalise', 'Kerluke', 'student', 13, 'female');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1350, 'Peetu', 'Honkanen', 'student', 14, 'male');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1351, 'Isto', 'Mustaää', 'teacher', 15, 'male');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1352, 'Lucija', 'Radić', 'student', 16, 'female');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1353, 'Lana', 'Božić', 'teacher', 17, 'female');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1354, 'Ela', 'Marić', 'student', 18, 'female');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1355, 'Niko', 'Kovač', 'student', 19, 'male');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1356, 'Clemmie', 'Lindgren', 'student', 20, 'male');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1357, 'Cale', 'Heidenreich', 'student', 21, 'male');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1358, 'Iina', 'Haapoja', 'student', 22, 'female');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1359, 'Inna', 'Eronen', 'student', 23, 'female');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1360, 'Peetu', 'Heikkinen', 'student', 24, 'male');
+INSERT INTO persons (person_id, first_name, last_name, role, adress_id, gender) VALUES (1361, 'Sami', 'Akkanen', 'student', 25, 'male');
 
